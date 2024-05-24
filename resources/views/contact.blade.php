77d8b9589a840168342b59a0565a0ced0f09e6b0
carlos ferrer <carlos.ferrer@davinci.edu.ar>
2024-05-24 10:36:51 -0300

    added routeing and template

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-test" >Contacto</h1>
+            <img src="{{ asset('assets/img/640x360.png') }}" alt="un texto">
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('/');
+
+Route::get('contacto', function () {
+    return view('contact');
+})->name('contacto');
